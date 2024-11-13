ec22872b048a84d9671f3445b2e766ec9f503e87
Juanjo García <juanvillaescusa72@gmail.com>
2024-11-13 12:16:58 +0100

    Sendable fixes

--- Sources/ReactiveStorage/ReactiveStorageProtocol.swift
+++ Sources/ReactiveStorage/ReactiveStorageProtocol.swift
@@ -7,9 +7,9 @@
 
 import Combine
 
-public typealias EntityType = Identifiable & Equatable
+public typealias EntityType = Identifiable & Equatable & Sendable
 
-public protocol ReactiveStorageProtocol {
+public protocol ReactiveStorageProtocol: Sendable {
     func getAllElementsObservable<Entity: EntityType>(of type: Entity.Type) -> AnyPublisher<[Entity], Never>
     func getSingleElementObservable<Entity: EntityType>(of type: Entity.Type, id: Entity.ID) -> AnyPublisher<Entity?, Never>
     func getAllElements<Entity: EntityType>(of type: Entity.Type) async -> [Entity]
